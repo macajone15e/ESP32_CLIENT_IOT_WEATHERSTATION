9bbb319d9a05cd6bc7ca4d28bb690b0d4683211c
Macajone Enzo <macajone.enzo@midmac.fr>
2026-04-18 15:28:14 +0200

    chore(main): update main with wifi functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include "config.hpp"
+#include "WiFiManager.hpp"
 
 void setup() {
   Serial.begin(115200);
+  setupWiFi();
   delay(5000);
   Serial.println("Starting...");
 }
 
 void loop() {
-
+  loopWiFi();
 }
