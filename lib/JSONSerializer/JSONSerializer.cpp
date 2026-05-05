e2a372a54775c14ac6d319833caa09289ad7e66f
Macajone Enzo <macajone.enzo@midmac.fr>
2026-05-05 12:10:44 +0200

    feat(JSON): add a serializer used before Mqttpublish

---
+++ lib/JSONSerializer/JSONSerializer.cpp
@@ -0,0 +1,16 @@
+#include "JSONSerializer.hpp"
+#include <Arduino.h>
+#include <ArduinoJson.h>
+
+JsonDocument doc;
+
+String generateJson(String id, String timestamp, float temperature, float altitude, float pressure) {
+  doc["id"] = id;
+  doc["timestamp"] = timestamp;
+  doc["temperature"] = temperature;
+  doc["altitude"] = altitude;
+  doc["pressure"] = pressure;
+  String jsonString;
+  serializeJson(doc, jsonString);
+  return jsonString;
+}
