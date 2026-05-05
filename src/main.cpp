505c15a676519ca6a8821c73f2b11aa37a3e6e5a
Macajone Enzo <macajone.enzo@midmac.fr>
2026-05-05 13:34:29 +0200

    chore(main): update main with every lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,35 @@
 #include <Arduino.h>
-#include "config.hpp"
-#include "WiFiManager.hpp"
+#include <config.hpp>
+#include <WiFiManager.hpp>
+#include <MQTTManager.hpp>
+#include <NTPManager.hpp>
+#include <I2CScanner.hpp>
+#include <DisplayManager.hpp>
+#include <SensorsManager.hpp>
+#include <JSONSerializer.hpp>
 
 void setup() {
   Serial.begin(115200);
+  setupI2C();
   setupWiFi();
+  setupMQTT();
+  setupNTP();
+  mqttConnect();
+  setupMonitor();
+  setupSensors();
   delay(5000);
   Serial.println("Starting...");
 }
 
 void loop() {
   loopWiFi();
+  loopNTP();
+  loopMQTT();
+  loopSensors();
+
+  if(isSensorsChange){
+    mqttPublish("Sensors", generateJson(getClientId(), getTimestamp(), temperature, altitude, pressure));
+    displayData(getFormattedTime(), temperature, pressure, altitude, false);
+    isSensorsChange = false;
+  }
 }
